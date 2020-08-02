0519f387152f810c3e057fe5a145cf455653a8a1
Johan Geluk <johan@vicompany.nl>
2020-08-02 15:19:39 +0200

    Add hardware reset support

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,35 @@
 extern crate panic_halt;
 
 use embedded_hal::blocking::spi::Write;
+use embedded_hal::blocking::delay::DelayMs;
 #[allow(deprecated)] // Required because enc28j60 depends on v1.
-use embedded_hal::digital::v1::{OutputPin};
+use embedded_hal::digital::v1::OutputPin;
+use embedded_hal::digital::v2::OutputPin as OutputPinV2;
+use embedded_hal::digital::v1_compat::OldOutputPin;
 
+use teensy4_bsp as bsp;
 use bsp::hal;
 use bsp::rt;
+
+use hal::iomuxc::spi::module::Module as SpiModule;
+
+use enc28j60::Enc28j60;
+
 use jnet::{ipv4, mac};
-use teensy4_bsp as bsp;
 
 const MAC: mac::Addr = mac::Addr([0x22, 0x22, 0x00, 0x00, 0x00, 0x00]);
 const IP: ipv4::Addr = ipv4::Addr([192, 168, 1, 101]);
 
-const SPI_BAUD_RATE_HZ: u32 = 1_000_000;
+//const SPI_BAUD_RATE_HZ: u32 = 1_000_000;
+const SPI_BAUD_RATE_HZ: u32 = 500_000;
 
 const KB: u16 = 1024;
+
+macro_rules! halt {
+    () => {
+        loop {}
+    };
+}
 
 #[rt::entry]
 fn main() -> ! {
@@ -28,6 +43,7 @@ fn main() -> ! {
         usb,
         spi,
         mut systick,
+        mut gpr,
         ..
     } = per;
 
@@ -69,19 +85,24 @@ fn main() -> ! {
     // This lets the chip select be controlled by the hardware,
     // and means that we won't need to pass a chip select pin
     // to the enc280j60, so we'll pass it a dummy instead.
-    //spi4.enable_chip_select_0(per.pins.p10.alt3());
+    spi4.enable_chip_select_0(per.pins.p10.alt3());
 
-    spi_test(&mut systick, spi4);
-    //net_setup(&mut systick, spi4);
+    // Create a GPIO output pin.
+    use hal::gpio::IntoGpio;
+    let op = per.pins.p9.alt5().into_gpio().fast(&mut gpr).output();
 
-    loop {}
+    //spi_test(&mut systick, spi4, op);
+    net_setup(&mut systick, spi4, op);
+
+    halt!();
 }
 
+// SPI testing code
 #[allow(unused)]
-fn spi_test<Mod, Delay>(delay: &mut Delay, mut spi: hal::spi::SPI<Mod>)
-where
-    Mod: hal::iomuxc::spi::module::Module,
-    Delay: embedded_hal::blocking::delay::DelayMs<u16>,
+fn spi_test<Mod>(
+    delay: &mut impl embedded_hal::blocking::delay::DelayMs<u16>,
+    mut spi: hal::spi::SPI<impl SpiModule>,
+    mut op: impl OutputPinV2)
 {
     // Dump some test data.
     loop {
@@ -95,29 +116,41 @@ where
                 }
             };
         }
-        delay.delay_ms(500);
+        op.set_low();
+        delay.delay_ms(250);
+        op.set_high();
+        delay.delay_ms(250);
     }
 }
 
 // ENC28J60 support (WIP)
 #[allow(deprecated, unused)] // 'deprecated' required because enc28j60 depends on v1.
-fn net_setup<Mod>(delay: &mut bsp::SysTick, spi: hal::spi::SPI<Mod>)
-where
-    Mod: hal::iomuxc::spi::module::Module,
+fn net_setup(
+    delay: &mut bsp::SysTick,
+    spi: hal::spi::SPI<impl SpiModule>,
+    mut rst: impl OutputPinV2<Error = impl core::fmt::Debug>)
 {
     struct DummyCS;
     impl OutputPin for DummyCS {
         fn set_high(&mut self) {}
         fn set_low(&mut self) {}
     }
-    let ncs = DummyCS;
 
+    log::info!("Initialising reset pin.");
+    if let Err(err) = rst.set_high() {
+        log::warn!("Failed to write to reset pin: {:?}", err);
+        halt!();
+    }
+    delay.delay_ms(10_u16);
+
+    let oop = OldOutputPin::new(rst);
+    
     log::info!("Setting up ENC28J60.");
-    let enc28j60 = enc28j60::Enc28j60::new(
+    let enc28j60 = Enc28j60::new(
         spi,
-        ncs,
+        DummyCS,
         enc28j60::Unconnected, // Interrupt
-        enc28j60::Unconnected, // Reset
+        oop,
         delay,
         7 * KB,
         MAC.0,
@@ -126,26 +159,29 @@ where
     match enc28j60 {
         Ok(mut enc) => {
             log::info!("ENC ready!");
-            return;
-            loop{
+            //return;
+            loop {
                 get_packets(&mut enc);
                 delay.delay(2000);
             }
         }
         Err(err) => {
             log::warn!("Failed to initialise ENC: {:?}", err);
-            return;
         }
     };
 }
 
 #[allow(deprecated)]
-fn get_packets<NCS, INT, RESET, Mod>(enc: &mut enc28j60::Enc28j60<hal::spi::SPI<Mod>, NCS, INT, RESET>)
-where
+fn get_packets<NCS, INT, RESET>(
+    enc: &mut Enc28j60<
+        hal::spi::SPI<impl SpiModule>,
+        NCS,
+        INT,
+        RESET>,
+) where
     NCS: OutputPin,
     INT: enc28j60::IntPin,
     RESET: enc28j60::ResetPin,
-    Mod: hal::iomuxc::spi::module::Module,
 {
     match enc.pending_packets() {
         Ok(pkt) => {
