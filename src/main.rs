69bfa2eee25729c6b40848e7b3ff77bc3d22105b
Johan Geluk <johan@geluk.io>
2020-11-19 18:08:09 +0100

    Update usage of teensy4 BSP to match latest API

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 
 extern crate panic_halt;
 
-use embedded_hal::blocking::spi::Write;
+use core::fmt::Write;
+
+// use embedded_hal::blocking::spi::Write;
+use cortex_m::interrupt::Mutex;
 use embedded_hal::blocking::delay::DelayMs;
 #[allow(deprecated)] // Required because enc28j60 depends on v1.
 use embedded_hal::digital::v1::OutputPin;
@@ -12,16 +15,9 @@ use embedded_hal::digital::v1_compat::OldOutputPin;
 
 use teensy4_bsp as bsp;
 use bsp::hal;
-use bsp::rt;
+use bsp::hal::iomuxc::prelude::*;
 
-use hal::iomuxc::spi::module::Module as SpiModule;
-
-use enc28j60::Enc28j60;
-
-use jnet::{ipv4, mac};
-
-const MAC: mac::Addr = mac::Addr([0x22, 0x22, 0x00, 0x00, 0x00, 0x00]);
-const IP: ipv4::Addr = ipv4::Addr([192, 168, 1, 101]);
+// use enc28j60::Enc28j60;
 
 //const SPI_BAUD_RATE_HZ: u32 = 1_000_000;
 const SPI_BAUD_RATE_HZ: u32 = 500_000;
@@ -34,39 +30,37 @@ macro_rules! halt {
     };
 }
 
-#[rt::entry]
+#[cortex_m_rt::entry]
 fn main() -> ! {
     // Take control of the peripherals.
     let mut per = bsp::Peripherals::take().unwrap();
-
-    let bsp::Peripherals {
-        usb,
-        spi,
-        mut systick,
-        mut gpr,
-        ..
-    } = per;
+    let core_per = cortex_m::Peripherals::take().unwrap();
 
     // Enable serial USB logging.
-    usb.init(Default::default());
+    let mut systick = bsp::SysTick::new(core_per.SYST);
+    let _ = bsp::usb::init(&systick, Default::default()).unwrap();
 
     // Set the default clock speed (500MHz).
     per.ccm
         .pll1
         .set_arm_clock(hal::ccm::PLL1::ARM_HZ, &mut per.ccm.handle, &mut per.dcdc);
 
+    log::info!("USB logging initialised.");
+
     // Configure the SPI clocks. We'll only use SPI4 for now.
-    let (_, _, _, spi4_builder) = spi.clock(
+    let (_, _, _, spi4_builder) = per.spi.clock(
         &mut per.ccm.handle,
         hal::ccm::spi::ClockSelect::Pll2,
         hal::ccm::spi::PrescalarSelect::LPSPI_PODF_5,
     );
 
+    let pins = bsp::t40::into_pins(per.iomuxc);
+
     // Set SPI pin assignments.
     let mut spi4 = spi4_builder.build(
-        per.pins.p11.alt3(),
-        per.pins.p12.alt3(),
-        per.pins.p13.alt3(),
+        pins.p11,
+        pins.p12,
+        pins.p13,
     );
 
     systick.delay(5000);
@@ -74,121 +68,121 @@ fn main() -> ! {
     // Set SPI clock speed.
     match spi4.set_clock_speed(hal::spi::ClockSpeed(SPI_BAUD_RATE_HZ)) {
         Ok(()) => {
-            log::info!("Set clock speed to {}Hz.", SPI_BAUD_RATE_HZ);
+            log::info!("Set SPI clock speed to {} Hz.", SPI_BAUD_RATE_HZ);
         }
         Err(err) => {
-            log::warn!("Unable to set clock speed: {:?}", err);
+            log::warn!("Unable to set SPI clock speed: {:?}", err);
         }
     }
 
     // Enable the peripheral-controlled chip select 0.
     // This lets the chip select be controlled by the hardware,
     // and means that we won't need to pass a chip select pin
-    // to the enc280j60, so we'll pass it a dummy instead.
-    spi4.enable_chip_select_0(per.pins.p10.alt3());
+    // to the enc28j60, so we'll pass it a dummy instead.
+    spi4.enable_chip_select_0(pins.p10);
 
     // Create a GPIO output pin.
-    use hal::gpio::IntoGpio;
-    let op = per.pins.p9.alt5().into_gpio().fast(&mut gpr).output();
+    let mut gpio = bsp::hal::gpio::GPIO::new(pins.p9).output();
+    gpio.set_fast(true);
 
-    //spi_test(&mut systick, spi4, op);
-    net_setup(&mut systick, spi4, op);
+    // spi_test(&mut systick, spi4, op);
+    //net_setup(&mut systick, spi4, op);
 
     halt!();
 }
 
-// SPI testing code
-#[allow(unused)]
-fn spi_test<Mod>(
-    delay: &mut impl embedded_hal::blocking::delay::DelayMs<u16>,
-    mut spi: hal::spi::SPI<impl SpiModule>,
-    mut op: impl OutputPinV2)
-{
-    // Dump some test data.
-    loop {
-        for i in 0..255_u8 {
-            match spi.write(&[i]) {
-                Ok(()) => {
-                    log::info!("Wrote byte: {}", i);
-                }
-                Err(err) => {
-                    log::warn!("Write failed: {:?}", err);
-                }
-            };
-        }
-        op.set_low();
-        delay.delay_ms(250);
-        op.set_high();
-        delay.delay_ms(250);
-    }
-}
+// // SPI testing code
+// #[allow(unused)]
+// fn spi_test<Mod>(
+//     delay: &mut impl embedded_hal::blocking::delay::DelayMs<u16>,
+//     mut spi: hal::spi::SPI<impl consts::Unsigned>,
+//     mut op: impl OutputPinV2)
+// {
+//     // Dump some test data.
+//     loop {
+//         for i in 0..255_u8 {
+//             match spi.write(&[i]) {
+//                 Ok(()) => {
+//                     log::info!("Wrote byte: {}", i);
+//                 }
+//                 Err(err) => {
+//                     log::warn!("Write failed: {:?}", err);
+//                 }
+//             };
+//         }
+//         op.set_low();
+//         delay.delay_ms(250);
+//         op.set_high();
+//         delay.delay_ms(250);
+//     }
+// }
 
-// ENC28J60 support (WIP)
-#[allow(deprecated, unused)] // 'deprecated' required because enc28j60 depends on v1.
-fn net_setup(
-    delay: &mut bsp::SysTick,
-    spi: hal::spi::SPI<impl SpiModule>,
-    mut rst: impl OutputPinV2<Error = impl core::fmt::Debug>)
-{
-    struct DummyCS;
-    impl OutputPin for DummyCS {
-        fn set_high(&mut self) {}
-        fn set_low(&mut self) {}
-    }
+// // ENC28J60 support (WIP)
+// #[allow(deprecated, unused)] // 'deprecated' required because enc28j60 depends on v1.
+// fn net_setup(
+//     delay: &mut bsp::SysTick,
+//     spi: hal::spi::SPI<impl consts::Unsigned>,
+//     mut rst: impl OutputPinV2<Error = impl core::fmt::Debug> + 'static)
+// {
+//     struct DummyCS;
+//     impl OutputPin for DummyCS {
+//         fn set_high(&mut self) {}
+//         fn set_low(&mut self) {}
+//     }
 
-    log::info!("Initialising reset pin.");
-    if let Err(err) = rst.set_high() {
-        log::warn!("Failed to write to reset pin: {:?}", err);
-        halt!();
-    }
-    delay.delay_ms(10_u16);
+//     log::info!("Initialising reset pin.");
+//     if let Err(err) = rst.set_high() {
+//         log::warn!("Failed to write to reset pin: {:?}", err);
+//         halt!();
+//     }
+//     delay.delay_ms(10_u16);
 
-    let oop = OldOutputPin::new(rst);
+//     let oop = OldOutputPin::new(rst);
     
-    log::info!("Setting up ENC28J60.");
-    let enc28j60 = Enc28j60::new(
-        spi,
-        DummyCS,
-        enc28j60::Unconnected, // Interrupt
-        oop,
-        delay,
-        7 * KB,
-        MAC.0,
-    );
-    log::info!("Setup done.");
-    match enc28j60 {
-        Ok(mut enc) => {
-            log::info!("ENC ready!");
-            //return;
-            loop {
-                get_packets(&mut enc);
-                delay.delay(2000);
-            }
-        }
-        Err(err) => {
-            log::warn!("Failed to initialise ENC: {:?}", err);
-        }
-    };
-}
+//     log::info!("Setting up ENC28J60.");
+//     let enc28j60 = Enc28j60::new(
+//         spi,
+//         DummyCS,
+//         enc28j60::Unconnected, // Interrupt
+//         oop,
+//         delay,
+//         7 * KB,
+//         MAC.0,
+//     );
+//     log::info!("Setup done.");
+//     match enc28j60 {
+//         Ok(mut enc) => {
+//             log::info!("ENC ready!");
+//             //return;
+//             loop {
+//                 get_packets(&mut enc);
+//                 delay.delay(2000);
+//             }
+//         }
+//         Err(err) => {
+//             log::warn!("Failed to initialise ENC: {:?}", err);
+//         }
+//     };
+// }
 
-#[allow(deprecated)]
-fn get_packets<NCS, INT, RESET>(
-    enc: &mut Enc28j60<
-        hal::spi::SPI<impl SpiModule>,
-        NCS,
-        INT,
-        RESET>,
-) where
-    NCS: OutputPin,
-    INT: enc28j60::IntPin,
-    RESET: enc28j60::ResetPin,
-{
-    match enc.pending_packets() {
-        Ok(pkt) => {
-            log::info!("{} packets ready to be decoded.", pkt);
-        }
-        Err(err) => {
-            log::warn!("Failed to get packet count from ENC: {:?}", err);
-        }
-    }
-}
+// #[allow(deprecated)]
+// fn get_packets<NCS, INT, RESET>(
+//     enc: &mut Enc28j60<
+//         hal::spi::SPI<impl consts::Unsigned>,
+//         NCS,
+//         INT,
+//         RESET>,
+// ) where
+//     NCS: OutputPin,
+//     INT: enc28j60::IntPin,
+//     RESET: enc28j60::ResetPin,
+// {
+//     match enc.pending_packets() {
+//         Ok(pkt) => {
+//             log::info!("{} packets ready to be decoded.", pkt);
+//         }
+//         Err(err) => {
+//             log::warn!("Failed to get packet count from ENC: {:?}", err);
+//         }
+//     }
+// }
