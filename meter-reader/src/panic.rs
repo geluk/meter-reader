d58cce690837e36b54fd76fd1f06037cd0bf77d5
Wim de With <wf@dewith.io>
2021-10-25 12:29:09 +0200

    Reset the board on panic in release mode

--- meter-reader/src/panic.rs
+++ meter-reader/src/panic.rs
@@ -1,8 +1,9 @@
-use core::{
-    panic::PanicInfo,
-    sync::atomic::{self, Ordering},
-};
+use core::panic::PanicInfo;
 
+#[cfg(debug_assertions)]
+use core::sync::atomic::{self, Ordering};
+
+#[cfg(debug_assertions)]
 #[inline(never)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -10,4 +11,11 @@ fn panic(info: &PanicInfo) -> ! {
     loop {
         atomic::compiler_fence(Ordering::SeqCst);
     }
+}
+
+#[cfg(not(debug_assertions))]
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    cortex_m::peripheral::SCB::sys_reset()
 }
